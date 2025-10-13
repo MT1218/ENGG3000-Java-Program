dde42f34bf968047858b3853878935d85793e605
Manav <manav.thobhani@gmail.com>
2025-10-14 01:09:34 +1100

    Wokwi Simulator works

--- app/src/main/java/mcp/App.java
+++ app/src/main/java/mcp/App.java
@@ -3,6 +3,7 @@ package mcp;
 public class App {
   private static final int RECEIVE_PORT_NUMBER = 3032;
   private static final int SEND_PORT_NUMBER = 3031;
+  // Put 127.0.0.1 for Wokwi Simulator
   private static final String SEND_IP_ADDR = "10.123.100.181";
 
   public static void main(String[] args) throws Exception {
